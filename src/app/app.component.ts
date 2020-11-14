686d3068bc6473f305a67a953aae11c8c2ad2e57
katreenapo <katja.djadechko1@gmail.com>
2020-11-14 18:05:59 +0200

    HW1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeW';
+  
+  tasks = [{
+    name: '24. Angular. Вступ',
+    isDone: true,
+    i: 1,
+  }]
+  addTask() {
+    this.tasks.push({
+      name: '24. Angular.',
+      isDone: true,
+      i: 3,
+    });
+    
+  }
 }
