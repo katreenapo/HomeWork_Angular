aa7f44844f62d000d208f49dc0746e1c7884dc19
katreenapo <katja.djadechko1@gmail.com>
2020-11-15 13:16:31 +0200

    HW1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeW';
-  
-  tasks = [{
-    name: '24. Angular. Вступ',
-    isDone: true,
-    i: 1,
-  }]
-  addTask() {
-    this.tasks.push({
-      name: '24. Angular.',
-      isDone: true,
-      i: 3,
-    });
-    
+  items = [];
+  newTask = ''; 
+ addToList() {
+    if (this.newTask == '') {
+    }
+    else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+    }
   }
+  
 }
