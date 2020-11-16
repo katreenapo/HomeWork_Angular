7f63fb42c83d8b36588acfb78ebd85839942c73a
katreenapo <katja.djadechko1@gmail.com>
2020-11-16 09:17:36 +0200

    HW1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  tasks = [ {
+  tasks = [{
     name: 'Завдання Angular Вступ',
     number: 'Task 1',
     isDone: true,
   }]
-  
+  addTask() {
+    this.tasks.push({
+      name: 'Завдання 2',
+      number: 'Task 2',
+      isDone: true,
+    })
+  }
 }
