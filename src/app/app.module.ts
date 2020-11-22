62678b48d4e79f523126a8ad67bad6459a098767
katreenapo <katja.djadechko1@gmail.com>
2020-11-22 16:49:56 +0200

    HW2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewListComponent } from './new-list/new-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule,
