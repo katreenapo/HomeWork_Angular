4705bebd6f7971972d4345cb096715d88bc1489e
katreenapo <katja.djadechko1@gmail.com>
2020-12-09 20:51:15 +0200

    Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HomeW';
-
-  nowDate = new Date();
-  price = 100;
-
-
-  users = [{
-    name: 'User1',
-    age: 20,
-  }, {
-    name: 'User2',
-    age: 30,
-  }]
-  size = 325.4545454;
 }
