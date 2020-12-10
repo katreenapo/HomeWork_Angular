7139f4b3c6e8d66b4625bf39cc569994036bdf8f
katreenapo <katja.djadechko1@gmail.com>
2020-12-10 18:01:48 +0200

    Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  task = '';
+  tasks =[];
+
+    addTask() {
+      console.log(this.tasks.push(this.task));
+  
+
+  }
 }
