0cbfe0624a6fb28f7d990adb6969651c79ed6192
katreenapo <katja.djadechko1@gmail.com>
2021-01-09 13:43:55 +0200

    delete

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -8,30 +8,30 @@ import { Todo } from '../models/service/todo.model';
   providedIn: 'root'
 })
 export class DataService {
-    urlAPI = environment.urlAPI; 
+  urlAPI = environment.urlAPI;
 
   constructor(
     private http: HttpClient
   ) { }
-getAllTodo (): Observable<Todo[]>{
-  return this.http.get<Todo[]>(this.urlAPI);
-    
+  getAllTodo(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.urlAPI);
+
   }
 
-  getPostById(id:number) {
+  getPostById(id: number) {
     return this.http.get<Todo>(`https://5fe21a3b7a94870017682086.mockapi.io/Todo/${id}`);
 
-}
-addNewTodo(todo: Todo): Observable<Todo> {
-  return this.http.post<Todo>(this.urlAPI, todo);
-}
+  }
+  addNewTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.urlAPI, todo);
+  }
 
-editTodo(todo: Todo, id: number): Observable<Todo> {
-  return this.http.put<Todo>(`${this.urlAPI}${id}`, todo);
-}
+  editTodo(todo: Todo, id: number): Observable<Todo> {
+    return this.http.put<Todo>(`${this.urlAPI}${id}`, todo);
+  }
 
-deleteTodo(id: number): Observable<Todo> {
-  return this.http.delete<Todo>(`${this.urlAPI}${id}`);
-}
+  deleteTodo(id: number): Observable<Todo> {
+    return this.http.delete<Todo>(`https://5fe21a3b7a94870017682086.mockapi.io/Todo/${id}`);
+  }
 }
 
