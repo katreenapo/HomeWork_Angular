9686205fd9ba93ad051d562d93887f39a25e1145
katreenapo <katja.djadechko1@gmail.com>
2021-01-19 21:05:19 +0200

    Project-todo

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/HomeW'));
+app.use(express.static(__dirname + '/dist/home-w'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/HomeW/index.html'));
+res.sendFile(path.join(__dirname+'/dist/home-w/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
